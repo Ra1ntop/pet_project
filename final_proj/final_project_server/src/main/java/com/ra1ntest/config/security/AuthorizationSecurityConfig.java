6519fa8d6ac8d55cee270d8982f41a4ca042fbaf
Ra1ntop <man111play@gmail.com>
2024-01-26 16:48:08 +0100

    day 13 of creating project 20th update ( |UPDATE| Config was updated )

--- final_proj/final_project_server/src/main/java/com/ra1ntest/config/security/AuthorizationSecurityConfig.java
+++ final_proj/final_project_server/src/main/java/com/ra1ntest/config/security/AuthorizationSecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static com.ra1ntest.persistance.type.PermissionType.*;
+import static com.ra1ntest.persistance.type.RoleType.ADMIN;
+import static com.ra1ntest.persistance.type.RoleType.CUSTOMER;
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.http.HttpMethod.PUT;
@@ -34,12 +36,15 @@ public class AuthorizationSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/open/**", "/api/auth/**").permitAll()
+                        .requestMatchers("/api/customer/**").hasAnyRole(CUSTOMER.name())
                         .requestMatchers(GET, "/api/customer/", "/api/customer/cart/", "/api/customer/order/", "/api/account/").hasAuthority(CUSTOMER_READ.name())
                         .requestMatchers(POST, "/api/customer/", "/api/account/").hasAuthority(CUSTOMER_CREATE.name())
                         .requestMatchers(PUT, "/api/customer/", "/api/customer/order/cancel-order/", "/api/customer/order/", "/api/account/").hasAuthority(CUSTOMER_UPDATE.name())
                         .requestMatchers(DELETE, "/api/customer/", "/api/account/").hasAuthority(CUSTOMER_DELETE.name())
 
-                        .requestMatchers(GET, "/api/admin", "/api/account/", "/api/admin/panel").hasAuthority(ADMIN_READ.name())
+
+                        .requestMatchers("/api/admin/**").hasAnyRole(ADMIN.name())
+                        .requestMatchers(GET, "/api/admin", "/api/account/", "/api/admin/panel/").hasAuthority(ADMIN_READ.name())
                         .requestMatchers(POST, "/api/admin/", "/api/account/").hasAuthority(ADMIN_CREATE.name())
                         .requestMatchers(PUT, "/api/admin/", "/api/account/").hasAuthority(ADMIN_UPDATE.name())
                         .requestMatchers(DELETE, "/api/admin/", "/api/account/").hasAuthority(ADMIN_DELETE.name())
